81384e9b134b37f6067a61027e3cd47d5bd0fb6e
Walter <@>
2015-04-08 09:39:11 +1200

    release CMCreator.jar

--- src/main/java/nzgo/toolkit/NZGOTVersion.java
+++ src/main/java/nzgo/toolkit/NZGOTVersion.java
@@ -12,7 +12,7 @@ public class NZGOTVersion extends Version {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "0.0.1";
+    private static final String VERSION = "0.1.0";
 
     private static final String DATE_STRING = "2013-2015";
 
@@ -28,27 +28,27 @@ public class NZGOTVersion extends Version {
         return "v" + VERSION + (IS_PRERELEASE ? " Prerelease " : "");
     }
 
-    public String[] getCredits() {
+    public String[] getCredits() { // TODO make diff to each tool
         return new String[]{
                 "Designed and developed by",
-                "Dong (Walter) Xie & Alexei J. Drummond",
+                "Dong (Walter) Xie",
                 "",
                 "Department of Computer Science",
                 "University of Auckland",
                 "walter@cs.auckland.ac.nz",
-                "alexei@cs.auckland.ac.nz",
                 "",
                 "Downloads, Help & Resources:",
                 "\thttps://github.com/CompEvol/NZGOT",
                 "",
                 "Source code distributed under the GNU Lesser General Public License:",
                 "\thttps://github.com/CompEvol/NZGOT.git",
-                "",
-                "Other developers:",
-                "\tThomas Hummel",
-                "",
-                "Thanks to:",
-                "\t"};
+//                "",
+//                "Other developers:",
+//                "\tThomas Hummel",
+//                "",
+//                "Thanks to:",
+//                "\t"
+        };
     }
 
     public String getHTMLCredits() {
